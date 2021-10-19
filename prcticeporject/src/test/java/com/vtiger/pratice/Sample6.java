aa9ea606b9da0a03d56ecaf17d866af332822dc6
mouli516 <mnadipena@gmail.com>
2021-10-19 13:13:52 +0530

    class 6

---
+++ prcticeporject/src/test/java/com/vtiger/pratice/Sample6.java
@@ -0,0 +1,11 @@
+package com.vtiger.pratice;
+
+public class Sample6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("class 6 created");
+
+	}
+
+}
